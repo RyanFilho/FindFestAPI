d637ee3f8abb077b2d1701ab18a2ce8b283d7f00
Ryan Sampaio Filho <ryanfilhoryanfilho@gmail.com>
2016-02-21 21:14:33 -0300

    Initial Compact Db and Event Model

---
+++ Find Fest API/Find Fest API/Data Acess Layer/DataBaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Find_Fest_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Fest_API.Data_Acess_Layer {
+    public class DataBaseContext : DbContext{
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ Find Fest API/Find Fest API/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Fest_API.Models {
+    [Table("Events")]
+    public class Event {
+        [Key]
+        public int EventID { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
